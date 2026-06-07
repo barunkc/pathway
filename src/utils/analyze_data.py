039c339e00ac1b253b91150b995581ba34c4e7b7
barunkc <barunkc657@gmail.com>
2026-06-07 12:04:27 +0545

    feat: handle csv encoding and dirty text values in analysis script

--- src/utils/analyze_data.py
+++ src/utils/analyze_data.py
@@ -1,8 +1,8 @@
 import os
+
 def cout_lines(file_path):
-    
     file = open(file_path, "r", encoding="utf-8")
-    header=file.readline()
+    header = file.readline()
     print(header)   
     a = 0
     for line in file:
@@ -10,5 +10,23 @@ def cout_lines(file_path):
     print("no of rows found are :", a)
     file.close()
 
-file_path = os.path.join("data","raw","plastic_waste.csv")
+def count_high_waste(file_path):
+    file = open(file_path, "r", encoding="utf-8")
+    high_waste = 0
+    header = file.readline()
+    
+    for line in file:
+        linedata = line.split(",")
+        volume = linedata[-1]
+        try:
+            float_volume = float(volume)
+            if float_volume > 50:
+                high_waste +=1
+        except ValueError:
+            continue
+    print("number of high waste found are :", high_waste)
+    file.close()
+
+file_path = os.path.join("data", "raw", "plastic_waste.csv")
 cout_lines(file_path)
+count_high_waste(file_path)
