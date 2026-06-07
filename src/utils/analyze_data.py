5005cc7ba42ec0a386a3764a023789609aa02c1a
barunkc <barunkc657@gmail.com>
2026-06-07 11:22:47 +0545

    Add file line counter utility script

--- src/utils/analyze_data.py
+++ src/utils/analyze_data.py
@@ -1,6 +1,14 @@
-file = open("data/raw/plastic_waste.csv", "r", encoding="utf-8")
-a = 0
-for line in file:
-    a += 1
-print("no of rows found are :", a)
-file.close()
+import os
+def cout_lines(file_path):
+    
+    file = open(file_path, "r", encoding="utf-8")
+    header=file.readline()
+    print(header)   
+    a = 0
+    for line in file:
+        a += 1
+    print("no of rows found are :", a)
+    file.close()
+
+file_path = os.path.join("data","raw","plastic_waste.csv")
+cout_lines(file_path)
